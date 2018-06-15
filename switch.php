6a6bb8bb92898e27a917a68ba9f3459049ee66c1
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2018-06-15 23:46:54 +0530

    added the switch stat

---
+++ switch.php
@@ -0,0 +1,17 @@
+<?php
+$number = 1;
+switch ($number) {
+    case 1:
+        echo "Number is 1";
+        break;
+    case label:
+        echo "Number is 2";
+        break;
+    case label:
+        echo "Number is 3";
+        break;
+    default:
+        echo "No condition is true. I reached at the dead end";
+}
+
+?>
