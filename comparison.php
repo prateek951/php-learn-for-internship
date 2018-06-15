d221dd791a756cf28d6875f8d193913f019a37c1
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2018-06-15 23:07:05 +0530

    comparison ops in php

---
+++ comparison.php
@@ -0,0 +1,14 @@
+<?php 
+
+// === checks equality and type(all operators are same as in JS)
+$number1 = '20';
+$number2 = 10;
+if($number1 == $number2){
+    echo "Equal"; //outputs in case of ==
+}else{
+    echo "Not equal";   //outputs in case of  ===
+}
+
+// and or operations for performing and/or operations
+
+?>
